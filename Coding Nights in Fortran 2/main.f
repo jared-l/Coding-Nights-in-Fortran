ce0eba3a1b6df99ef87ccd29d19842fa814dba46
jared-l <>
2015-07-01 18:58:40 -0500

    Initial commit

---
+++ Coding Nights in Fortran 2/main.f
@@ -0,0 +1,12 @@
+C
+C File:   main.f
+C Author: hclambda
+C
+C Created on July 1, 2015, 6:33 PM
+C
+      program main
+          print *, 'Hello World'
+      end
+      
+      subroutine initialize_hamiltonian(array, dim)
+      end subroutine

5d489feac9b567e1f4d427b7a36de5b45d9229cb
hclambda <jaredlalmansingh@gmail.com>
2015-07-01 20:23:10 -0500

    Created the functions to initialize the hamiltonian and print it

--- Coding Nights in Fortran 2/main.f
+++ Coding Nights in Fortran 2/main.f
@@ -5,8 +5,44 @@ C
 C Created on July 1, 2015, 6:33 PM
 C
       program main
-          print *, 'Hello World'
+          integer rows, columns
+          parameter (rows=7)
+          parameter (columns=7)
+          
+          ! Create the hamiltonian array
+          real hamiltonian(rows, columns)
+          
+          call initialize_hamiltonian(hamiltonian, rows, columns)
+          call print_hamiltonian(hamiltonian, rows, columns)
       end
       
-      subroutine initialize_hamiltonian(array, dim)
+      subroutine initialize_hamiltonian(hamiltonian, rows, columns)
+          integer rows, columns, scoeff, coeff1, coeff2
+          real hamiltonian(rows, columns)
+          
+          scoeff = (rows - 1)/2
+          
+          do i=1,rows
+              do j=1, columns
+                  ! Fill these in with slater determinants
+                  hamiltonian(i,j) = -1
+                  coeff1 = i - j + scoeff + 2
+                  coeff2 = j - i + scoeff + 2
+                  
+                  if (coeff1 .lt. scoeff .or. coeff2 .lt. scoeff)
+     +            then
+                      hamiltonian(i,j) = 0
+                  endif
+                  
+              end do
+          end do
+      end subroutine
+      
+      subroutine print_hamiltonian(hamiltonian, rows, columns)
+          integer rows, columns
+          real hamiltonian(rows, columns)
+          
+          do, i=1, rows
+              write(*,*) (hamiltonian(i,j), j=1, columns)
+          enddo
       end subroutine

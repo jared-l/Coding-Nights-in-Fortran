2a773fabf02a1d1a45d119e4990acbed97b935d8
hclambda <jaredlalmansingh@gmail.com>
2015-07-01 20:43:18 -0500

    Added some more detailed comments.

--- Coding Nights in Fortran 2/main.f
+++ Coding Nights in Fortran 2/main.f
@@ -9,29 +9,43 @@ C http://nucleartalent.github.io/Course2ManyBodyMethods/doc/pub/fci/pdf/fci-prin
 C
       program main
           integer rows, columns
+          real eigen_value
           parameter (rows=7)
           parameter (columns=7)
+          parameter (eigen_value=-1.0)      ! Define the eigen_value
+          real hamiltonian(rows, columns)   ! Create the Hamiltonian array
           
-          ! Create the hamiltonian array
-          real hamiltonian(rows, columns)
-          
-          call initialize_hamiltonian(hamiltonian, rows, columns)
-          call print_hamiltonian(hamiltonian, rows, columns)
+          ! Initialize the Hamiltonian
+          call initialize_hamiltonian(hamiltonian,
+     +                                rows,
+     +                                columns, 
+     +                                eigen_value)
+                                      
+          ! Print to see that our results are correct
+          call print_hamiltonian(hamiltonian, 
+     +                           rows, 
+     +                           columns)
       end
       
       
 C     This subroutine initializes the Hamiltonian to match the configuration shown on page 5 of
 C     http://nucleartalent.github.io/Course2ManyBodyMethods/doc/pub/fci/pdf/fci-print.pdf
-      subroutine initialize_hamiltonian(hamiltonian, rows, columns)
+      subroutine initialize_hamiltonian(hamiltonian, 
+     +                                  rows, 
+     +                                  columns, 
+     +                                  eigenvalue)
+                                        
           integer rows, columns, midpt, coeff1, coeff2
-          real hamiltonian(rows, columns)
+          real hamiltonian(rows, columns), eigenvalue
           
           midpt = (rows - 1)/2
           
+          ! This works by looping over the 2-d array, and checking to see whether or
+          ! not to set the array value to some particular eigen-value.
           do i=1,rows
               do j=1, columns
                   ! Fill these in with slater determinants
-                  hamiltonian(i,j) = -1
+                  hamiltonian(i,j) = eigenvalue
                   coeff1 = i - j + midpt + 2
                   coeff2 = j - i + midpt + 2
                   

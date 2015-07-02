3cdfc88774a677074c2863cb227cd9ba846d0dee
hclambda <jaredlalmansingh@gmail.com>
2015-07-01 20:33:53 -0500

    Modified the Hamiltonian to correctly match the sample Hamiltonian given on page 5. Also added some more detailed comments.

--- Coding Nights in Fortran 2/main.f
+++ Coding Nights in Fortran 2/main.f
@@ -1,8 +1,11 @@
 C
 C File:   main.f
-C Author: hclambda
+C Author: jared-l
 C
 C Created on July 1, 2015, 6:33 PM
+C      
+C Description: This program attempts to follow and solve the problems shown and described in
+C http://nucleartalent.github.io/Course2ManyBodyMethods/doc/pub/fci/pdf/fci-print.pdf
 C
       program main
           integer rows, columns
@@ -16,6 +19,9 @@ C
           call print_hamiltonian(hamiltonian, rows, columns)
       end
       
+      
+C     This subroutine initializes the Hamiltonian to match the configuration shown on page 5 of
+C     http://nucleartalent.github.io/Course2ManyBodyMethods/doc/pub/fci/pdf/fci-print.pdf
       subroutine initialize_hamiltonian(hamiltonian, rows, columns)
           integer rows, columns, midpt, coeff1, coeff2
           real hamiltonian(rows, columns)
@@ -32,17 +38,23 @@ C
                   if (coeff1 .lt. midpt .or. coeff2 .lt. midpt)
      +            then
                       hamiltonian(i,j) = 0
+                  else if (i .eq. 1 .and. j .eq. 2) then
+                      hamiltonian(i,j) = 0
+                  else if (i .eq. 2 .and. j .eq. 1) then
+                      hamiltonian(i,j) = 0
                   endif
                   
               end do
           end do
       end subroutine
       
+      
+C     This subroutine prints the Hamiltonian (useful for debugging)
       subroutine print_hamiltonian(hamiltonian, rows, columns)
           integer rows, columns
           real hamiltonian(rows, columns)
           
           do, i=1, rows
-              write(*,*) (hamiltonian(i,j), j=1, columns)
+              write(*,*) (hamiltonian(i,j), j=1, columns) ! N.B.: this uses an 'implicit' `do` loop!
           enddo
       end subroutine
